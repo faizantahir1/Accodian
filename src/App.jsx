3e84f6f13d7103d0b6561ec306b541f558f2e650
faizan <iamfaizantahir01@gmail.com>
2024-04-30 00:04:12 -0700

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Birthday from './Birthday';
+
+const App = () => {
+  return (
+    <>
+    <Birthday/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const bday = [
+    {
+    id:8 ,
+    name:'Eleanor',
+    age:'10',
+    image:'https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg'},
+    {
+        id:10 ,
+        name:'Ricky',
+        age:'22',
+        image:"https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png"
+        },
+        {
+            id:11 ,
+            name:'Jeremiah',
+            age:'20',
+            image:'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg'
+            },
+            {
+                id:5 ,
+                name:'Alma',
+                age:'17',
+                image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTNPhyPkgMe2Rgj2VGETytO9w1S25jagOCalruEF-yxg&s'
+                },
+                {
+                    id:7 ,
+                    name:'Faizan Abbasi',
+                    age:'24',
+                    image:'https://yt3.ggpht.com/NY5-GS1Mc7XjQhn49E6BEHoYFsyFpUa5YU-IrdnjXcztE8UlkwGHdFOoVobq3yKpHSaYQnKh=s88-c-k-c0x00ffffff-no-rj'
+                    },
+
+]
